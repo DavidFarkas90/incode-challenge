9207f85af8e419730ee3adb47cf8d5c20ae35dc0
David Farkas <davidfarkas90@gmail.com>
2026-04-22 12:08:02 +0200

    Added page URLs

---
+++ src/constants/page-urls.ts
@@ -0,0 +1,7 @@
+export const PAGE_URLS = {
+  LOGIN: "https://demo-dashboard.incode.com/log-in",
+  HOME: "https://demo-dashboard.incode.com",
+  FLOWS: "https://demo-dashboard.incode.com/flows",
+  IDENTITIES: "https://demo-dashboard.incode.com/identities",
+  SESSIONS: "https://demo-dashboard.incode.com/sessions",
+};
