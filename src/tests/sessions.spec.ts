027f2886b5ccac62da78be81028db7686a86f2e6
David Farkas <davidfarkas90@gmail.com>
2026-04-22 16:48:09 +0200

    Added initial version of sessions test

---
+++ src/tests/sessions.spec.ts
@@ -0,0 +1,52 @@
+import "dotenv/config";
+import { test, expect } from "@playwright/test";
+import { PAGE_URLS } from "../constants/page-urls";
+import { LoginPage } from "../pages/login/login.page";
+import { BasePage } from "../pages/base/base.page";
+import { Labels } from "../constants/labels";
+import { SessionsPage } from "../pages/sessions/sessions.page";
+import { SingleSessionPage } from "../pages/sessions/single-session.page";
+
+let sessionsPage: SessionsPage;
+let singleSessionPage: SingleSessionPage;
+let basePage: BasePage;
+const USER_NAME = "David Farkas";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(PAGE_URLS.LOGIN);
+  await expect(page).toHaveURL(PAGE_URLS.LOGIN);
+  const loginPage = new LoginPage(page);
+  await loginPage.login(process.env.USER_EMAIL!, process.env.USER_PASSWORD!);
+  await expect(page).toHaveURL(PAGE_URLS.HOME);
+});
+
+test("assert user name in single session page", async ({ page }) => {
+  await test.step("Navigate to Sessions page", async () => {
+    basePage = new BasePage(page);
+    expect(await basePage.isMainNavigationVisible()).toBe(true);
+
+    await basePage.navigateTo(Labels.SESSIONS);
+    await expect(page).toHaveURL(PAGE_URLS.SESSIONS);
+    sessionsPage = new SessionsPage(page);
+  });
+
+  await test.step("Verify sessions table and user session row is visible", async () => {
+    expect(await sessionsPage.isSessionsTableVisible()).toBe(true);
+    const sessionRow = await sessionsPage.getSessionRowByName(USER_NAME);
+    await expect(sessionRow).toBeVisible();
+  });
+
+  await test.step("Navigate to single session page and verify session info title", async () => {
+    await sessionsPage.clickOnSessionRowByName(USER_NAME);
+    singleSessionPage = new SingleSessionPage(page);
+    const sessionTitle = await singleSessionPage.getSessionInfoTitle();
+    expect(sessionTitle).toContain(Labels.SESSION_INFO);
+  });
+
+  await test.step("Verify ID OCR table and content", async () => {
+    const idOCRTableVisible = await singleSessionPage.isIdOCRTableVisible();
+    expect(idOCRTableVisible).toBe(true);
+    const userNameInTable = await singleSessionPage.getIdOCRContentByLabel(Labels.FULL_NAME_OCR);
+    expect(userNameInTable).toBe(USER_NAME);
+  });
+});
