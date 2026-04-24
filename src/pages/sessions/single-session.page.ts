f0c4b8ebd9a270f8f1bb23f62d7f4267a32efd13
David Farkas <davidfarkas90@gmail.com>
2026-04-24 12:11:20 +0200

    Added new page objects for single session page

--- src/pages/sessions/single-session.page.ts
+++ src/pages/sessions/single-session.page.ts
@@ -7,6 +7,9 @@ export class SingleSessionPage {
   readonly sessionInfoTitle: Locator;
   readonly addFaceToDatabaseButton: Locator;
   readonly faceInDatabaseLabel: Locator;
+  readonly hamburgerMenuIcon: Locator;
+  readonly menuList: Locator;
+  readonly menuListItem: (menuItem: string) => Locator;
   // ID OCR table and content
   readonly idOCRTable: Locator;
   readonly idOCRContent: Locator;
@@ -17,6 +20,12 @@ export class SingleSessionPage {
     this.sessionInfoTitle = page.getByText(Labels.SESSION_INFO);
     this.addFaceToDatabaseButton = page.getByTestId("button-icon-iconBtnAdd");
     this.faceInDatabaseLabel = page.locator(".status-set", { hasText: Labels.FACE_IN_DATABASE });
+    this.hamburgerMenuIcon = page.getByRole("button", { name: "menu icon" });
+    this.menuList = page.locator("ul.menu-list__list");
+    this.menuListItem = (menuItem: string) =>
+      page
+        .locator("ul.menu-list__list")
+        .filter({ has: page.getByRole("listitem", { name: menuItem }) });
     this.idOCRTable = page.getByText(Labels.ID_OCR);
     this.idOCRContent = page.locator(".id-info .dinamic-field");
     this.tableCellContent = page.locator(".content");
@@ -34,18 +43,24 @@ export class SingleSessionPage {
     return this.faceInDatabaseLabel;
   }
 
+  async getHamburgerMenuIcon(): Promise<Locator> {
+    return this.hamburgerMenuIcon;
+  }
+
+  async getMenuList(): Promise<Locator> {
+    return this.menuList;
+  }
+
+  async getMenuListItem(menuItem: string): Promise<Locator> {
+    return await this.menuListItem(menuItem);
+  }
+
   async getIdOCRTable(): Promise<Locator> {
     return this.idOCRTable;
   }
 
   async getIdOCRContent(): Promise<Locator> {
     return this.idOCRContent;
-  }
-
-  async isIdOCRTableVisible(): Promise<boolean> {
-    await (await this.getIdOCRTable()).scrollIntoViewIfNeeded();
-    await (await this.getIdOCRTable()).waitFor({ state: "visible" });
-    return true;
   }
 
   async getIdOCRContentByLabel(text: string): Promise<string> {
@@ -55,5 +70,29 @@ export class SingleSessionPage {
       .filter({ has: this.page.getByRole("heading", { name: text }) })
       .locator(".content")
       .innerText();
+  }
+
+  async clickAddFaceToDatabaseButton(): Promise<void> {
+    await this.addFaceToDatabaseButton.click();
+  }
+
+  async clickOnHamburgerMenu(): Promise<Locator> {
+    // TODO: Update return type
+    await this.hamburgerMenuIcon.click();
+    return this.menuList;
+  }
+
+  async clickOnMenuItem(menuItem: string): Promise<void> {
+    await this.menuListItem(menuItem).click();
+  }
+
+  async isAddFaceToDatabaseDisabled(): Promise<boolean> {
+    return await this.addFaceToDatabaseButton.isDisabled();
+  }
+
+  async isIdOCRTableVisible(): Promise<boolean> {
+    await (await this.getIdOCRTable()).scrollIntoViewIfNeeded();
+    await (await this.getIdOCRTable()).waitFor({ state: "visible" });
+    return true;
   }
 }
