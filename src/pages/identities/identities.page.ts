5f899fa7f86e2241df56eb9408ee9d5ce63f3e6e
David Farkas <davidfarkas90@gmail.com>
2026-04-24 11:11:32 +0200

    Added Identities page objects

---
+++ src/pages/identities/identities.page.ts
@@ -0,0 +1,42 @@
+import { Page, Locator } from "@playwright/test";
+import { Labels } from "../../constants/labels";
+
+export class IdentitiesPage {
+  readonly page: Page;
+  readonly identitiesTitle: Locator;
+  readonly moreActionsButton: Locator;
+  readonly identitiesTable: Locator;
+  readonly identitiesRow: (name: string, identity: string) => Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.identitiesTitle = page.getByRole("heading", { name: Labels.IDENTITIES });
+    this.moreActionsButton = page.getByRole("button", { name: Labels.ACTIONS });
+    this.identitiesTable = page.getByRole("table");
+    this.identitiesRow = (name: string, identity: string) =>
+      page
+        .getByRole("row")
+        .filter({ has: page.getByRole("cell", { name: name, exact: true }) })
+        .filter({ has: page.getByRole("cell", { name: identity, exact: true }) });
+  }
+
+  async getIdentitiesTitle(): Promise<Locator> {
+    return await this.identitiesTitle;
+  }
+
+  async getMoreActionsButton(): Promise<Locator> {
+    return await this.moreActionsButton;
+  }
+
+  async getIdentitiesTable(): Promise<Locator> {
+    return await this.identitiesTable;
+  }
+
+  async getIdentitiesRowByParams(name: string, identity: string): Promise<Locator> {
+    return this.identitiesRow(name, identity);
+  }
+
+  async clickOnMoreActionsButton(): Promise<void> {
+    await this.moreActionsButton.click();
+  }
+}
