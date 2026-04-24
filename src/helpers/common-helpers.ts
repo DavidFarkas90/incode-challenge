811a2c350815dc098a4567d10f29c4904731eed9
David Farkas <davidfarkas90@gmail.com>
2026-04-24 17:05:12 +0200

    Added new helper for title case

--- src/helpers/common-helpers.ts
+++ src/helpers/common-helpers.ts
@@ -5,3 +5,11 @@ export function generateRandomNumbers(): number {
 export function getRandomElement<T>(array: T[]): T {
   return array[Math.floor(Math.random() * array.length)];
 }
+
+export function toTitleCase(title: string): string {
+  return title
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
